ce9db291ecfe7518933f86fd427938d9cf6d9a9b
Igor <igorko.by@gmail.com>
2020-01-21 13:18:40 +0300

    Finish project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// BOOTSTRAP
+// import 'bootstrap/dist/js/bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
+// JS
+// import './js/'
+
+// SCSS
+import './assets/scss/main.scss'
+
+// CSS
+// import './assets/css/main.css'
